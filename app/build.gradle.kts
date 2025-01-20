4d3992c383630a443ca8659010a1ad449a326e56
gusviradigital <agus@gusviradigital.co.id>
2025-01-20 10:54:20 +0700

    Enable ViewBinding in build.gradle.kts, update navigation and preference dependencies, modify ic_save.xml for color consistency, and enhance strings.xml with new and updated string resources for improved user experience.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,13 +33,18 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.navigation.fragment)
+    implementation(libs.androidx.navigation.ui)
+    implementation(libs.androidx.preference)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
